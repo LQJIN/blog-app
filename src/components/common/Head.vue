0fd91a3a36ef480551489af5b3e9116041c7c9ef
LQJIN <isjinliqiang@foxmail.com>
2017-09-15 22:06:11 +0800

    add Head.vue

--- src/components/common/Head.vue
+++ src/components/common/Head.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="head">
-
+    <div class="nav">
+      <span>ooo</span>
+      <h2>BLOG</h2>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+  .nav { position: relative;
+    top: 0; left: 0; width: 100%;
+    height: 1rem;  background: green;}
 </style>
 
 <script>
-  
+
 </script>
