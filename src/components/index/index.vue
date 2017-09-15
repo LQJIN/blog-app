e7d4b81df36a0716bf6058755e9bd29b66d392eb
LQJIN <isjinliqiang@foxmail.com>
2017-09-15 22:37:00 +0800

    add Head.vue

---
+++ src/components/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <Head></Head>
+    <router-view></router-view>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+  import Head from "../common/Head.vue"
+  import Foot from "../common/foot.vue"
+
+  export default {
+    name: 'app',
+    components: {
+      Head,
+      Foot
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
