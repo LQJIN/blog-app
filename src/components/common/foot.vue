ed407970791ac20f6ecd86bb2dcfb6bdf9355955
LQJIN <isjinliqiang@foxmail.com>
2017-09-16 21:04:24 +0800

    update foot.vue

--- src/components/common/foot.vue
+++ src/components/common/foot.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="foot fixed">
     <ul>
-      <li><router-link to="/">Blog</router-link></li>
-      <li><router-link to="/me">Me</router-link></li>
+      <li><router-link to="/"><span class="glyphicon glyphicon-list-alt"></span> Blog</router-link></li>
+      <li><router-link to="/"><span class="glyphicon glyphicon-user"></span> Me</router-link></li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-  li a{display: inline-block; width: 100%;height: 100%;}
+  li a{display: inline-block; width: 100%;height: 100%; color: #0d4473;}
   .foot{position:fixed;width: 100%; bottom: 0;text-align: center;}
   .fiexd{position: fixed;}
-  ul{display: flex;height: 0.6rem;line-height: 0.6rem;text-align: center;}
-  ul li{flex: 1;background-color: #f1f1f1;}
-  ul li:nth-child(1){background-color: #f1f1f1; border-right: 0.25px solid darkslategray }
-  .isIndex li:nth-child(1){background-color: #d3d3d3;}
-  .isIndex li:nth-child(1) a{color: #41b883;}
-  .isManage li:nth-child(2){background-color: #d3d3d3;}
-  .isManage li:nth-child(2) a{color: #41b883;}
+  ul{display: flex;height: 0.8rem;line-height: 0.8rem;text-align: center;}
+  ul li{flex: 1;  background-color: #dee2e3;}
+  ul li:nth-child(1){background-color: #dee2e3;
+    border-right: 0.25px solid darkslategray }
 </style>
 
 <!-- 可以看到foot.vue使用了<router-link>标签，该标签主要实现跳转链接功能，
