f69914cd5f05e9ccba4e567ea0a10c8783864ea4
LQJIN <isjinliqiang@foxmail.com>
2017-09-16 21:05:03 +0800

    update head.vue

--- src/components/common/Head.vue
+++ src/components/common/Head.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="head">
-    <div class="nav">
-      <span class="glyphicon glyphicon-search">ooo</span>
-    </div>
-    <div class="title">
+      <slot>
+        <span v-on="sideBar" class="glyphicon glyphicon-menu-hamburger"></span>
+      </slot>
+
       <h2>BLOG</h2>
-    </div>
   </div>
 </template>
 
 <style scoped>
-  .head { position: relative;
-    top: 0; left: 0; width: 100%;
-    height: 1rem;  background: green;}
+  .head { display: flex;  height: 1rem;  color: #fff;
+          position: fixed;  top: 0;  width: 100%; background: #0d4473}
+  .head span{ width: 1rem;  height: 1rem; line-height: 1rem;
+    text-align:center; font-size: 20px; color: #9d9d9d; }
+  .head h2 { flex: 1;  text-align: center;
+             text-indent: -1rem; line-height: 1rem;}
 </style>
+<script>
+  export default {
+    methods: {
+      sideBar: function () {
+        history.go(-1);
+      }
+    }
+  }
+</script>
