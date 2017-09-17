d6f0868f559bc4c2d0a65132e10b178733b3e8f9
LQJIN <isjinliqiang@foxmail.com>
2017-09-17 15:36:57 +0800

    update head.vue

--- src/components/common/Head.vue
+++ src/components/common/Head.vue
@@ -9,13 +9,11 @@
 </template>
 
 <style scoped>
-  .head { display: flex;  height: 1rem;  color: #fff;
-          position: fixed;  top: 0;  width: 100%; background: #0d4473}
-  .head span{ width: 1rem;  height: 1rem; line-height: 1rem;
-    text-align:center; font-size: 20px; color: #9d9d9d; }
-  .head h2 { flex: 1;  text-align: center;
-             text-indent: -1rem; line-height: 1rem;}
+  .head { display: flex; height: 1rem; color: #fff; position: fixed; top: 0; width: 100%; background: #0d4473}
+  .head span{ width: 1rem; height: 1rem; line-height: 1rem; text-align:center; font-size: 0.5rem; color: #9d9d9d; }
+  .head h2 { flex: 1; text-align: center; text-indent: -1rem; line-height: 1rem; font-size: 0.5rem;}
 </style>
+
 <script>
   export default {
     methods: {
