5ce71343fb2508d041c2cb049af7f1906c89f10f
LQJIN <isjinliqiang@foxmail.com>
2017-09-18 18:35:31 +0800

    update foot.vue

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,23 +1,31 @@
 <template>
-  <div id="index">
+  <div class="index">
     <!--<img src="./assets/logo.png">-->
     <Headd></Headd>
+    <index-blog></index-blog>
+    <index-blog></index-blog>
+    <index-blog></index-blog>
+    <index-blog></index-blog>
+    <index-blog></index-blog>
+    <index-blog></index-blog>
     <foot></foot>
   </div>
 </template>
 
+<style scoped>
+  .index{  margin-top: 1rem; margin-bottom: 0.7rem;}
+</style>
+
 <script>
   import Headd from "../common/Head.vue"
   import Foot from "../common/foot.vue"
+  import IndexBlog from "./IndexBlog.vue"
 
   export default {
     components: {
       Headd,
-      Foot
+      Foot,
+      IndexBlog
     }
   }
 </script>
-
-<style scoped>
-
-</style>
