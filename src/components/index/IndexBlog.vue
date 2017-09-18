37a2604b5d8ca5e4282de1a9b8634a448d4ab9f0
LQJIN <isjinliqiang@foxmail.com>
2017-09-18 18:34:03 +0800

    add IndexBlog.vue

---
+++ src/components/index/IndexBlog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="index-blog">
+    <div class="text">
+      <h5>Ignoring China's security issue not in ROK's interest:China Daily editional</h5>
+      <ul>
+        <li class="tag">OPINION</li>
+        <li class="date">September 14,2017</li>
+        <li class="comment"><span class="glyphicon glyphicon-comment"></span> 6</li>
+      </ul>
+    </div>
+    <div class="img">
+      <img src="../../assets/img/p4.png" alt="" />
+    </div>
+  </div>
+</template>
+<style scoped>
+  .index-blog{display: flex; padding: 0.2rem 0.2rem 0.2rem 0.2rem;}
+  .text{ flex: 3.5; float: left; text-align: left; font-size: 0.2rem;}
+  .text h5{font-size: 0.2rem; font-weight:bold; margin-bottom: 0.1rem;}
+  .text ul li{display: inline-block; margin-right: 0.05rem;}
+  .text ul li span{z-index: -1;}
+  .img{flex: 1; float: right;}
+  .tag{color: #023b6b;}
+  .date{color: #b9b9b9;}
+  .comment{color: #104775;}
+</style>
