6a24b490fbda1e11222216d795dd82c7c772cf56
LQJIN <isjinliqiang@foxmail.com>
2017-09-19 18:55:46 +0800

    update BlogCommentAdd.vue

--- src/components/common/BlogCommentAdd.vue
+++ src/components/common/BlogCommentAdd.vue
@@ -89,7 +89,7 @@
                 </div>
                 <div class="comment-sent">
                   <form action="" method="get">
-                    <input type="text" title="comment" placeholder="用户名或邮件地址"/>
+                    <input type="text" title="comment" placeholder="make comments"/>
                     <button type="submit">Send</button>
                   </form>
                 </div>
@@ -104,7 +104,7 @@
     border-bottom: 0.02rem solid #a2b7b8;  z-index: 1;}
   .content-head span{ width: 1rem; height: 1rem; line-height: 1rem; text-align:center; font-size: 0.5rem; color: #0d4473;}
   .content-head h4 { flex: 1; text-align: center; text-indent: -1rem; line-height: 1rem; font-size: 0.5rem; color: #0d4473;}
-  .comment-li {margin: 0.92rem 0.2rem 0.7rem 0.2rem; }
+  .comment-li {margin: 0.92rem 0.2rem 1.2rem 0.2rem; }
   .comment-li ul li{ position: relative; padding: 0.2rem 0 0 0; display: inline-block; font-size: 0.2rem;}
   .comment-li img{ left: 0; top: 0; width: 1rem; height: 1rem; float: left; margin: 0.1rem 0.2rem 0 0;
     text-align: center; border-radius: 50%;}
@@ -115,8 +115,11 @@
   .comments .name{ float: left;}
   .comments .date{ float: right;}
 
-  .comment-sent{ position: fixed; bottom: 0; width: 100%; height: 1.2rem; background: #eeeeee;}
-
+  .comment-sent{ position: fixed; bottom: 0; width: 100%; height: 1.2rem; background: #eeeeee; }
+  .comment-sent form{margin: 0.2rem 0.2rem 0.2rem 0.22rem;}
+  .comment-sent input{ width: 4.2rem; height: 0.8rem; font-size: 0.22rem; border: none; padding-left: 0.2rem;}
+  .comment-sent button{ width: 1.4rem; height: 0.8rem; background: #1c4d9c;
+    border: none; color: #ffffff; margin-left: 0.1rem; font-size: 0.25rem;}
   /*.blog-comment-add{position: fixed; top: 0;left: 0;width: 100%;height: 100%;}
   .comment-content{position: absolute;z-index: 200;width: 6rem; height: 5rem; box-sizing: border-box;left: 50%;top: 50%;margin-left: -3rem;margin-top: -2rem; padding: 0.1rem 0.1rem; background: #fff; box-shadow: 0 10px 15px rgba(0,0,0,0.2); text-align: center;}
   #pop-content h1{color: #555; padding-bottom: 0.2rem; border-bottom: 1px solid #DDD;}
