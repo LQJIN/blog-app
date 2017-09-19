091d97c6cd2b0081a0cec5f110b57068242c3f03
LQJIN <isjinliqiang@foxmail.com>
2017-09-19 19:52:10 +0800

    add Me.vue MeHead.vue

---
+++ src/components/index/Me.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="me">
+    <me-head></me-head>
+    <foot></foot>
+  </div>
+</template>
+
+
+<script>
+  import MeHead from '../common/MeHead.vue'
+  import Foot from "../common/foot.vue"
+
+  export default {
+    components:{
+      MeHead,
+      Foot
+    }
+  }
+</script>
