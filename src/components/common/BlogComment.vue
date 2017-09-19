5c987f647420f906579107b3b8fa3b9528c3fa5f
LQJIN <isjinliqiang@foxmail.com>
2017-09-19 11:30:06 +0800

    update BlogDetail.vue Blog.vue add BlogComment.vue

---
+++ src/components/common/BlogComment.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="blog-comment">
+    <h5 class="blog-comm-nav">
+      Latest Comments
+    </h5>
+    <div class="user-comment">
+      <ul>
+        <li>
+          <img src="../../assets/img/p6.png" alt="">
+          <div class="comments">
+            <div class="name-date">
+              <div class="name">user的名子</div>
+              <div class="date">2017-09-18 15:58</div>
+            </div>
+            <div class="comment-words">
+              <p>The business magnate also shared with people how Alibaba has grown into what it is today.
+                The business magnate also shared with people how Alibaba has grown into what it is today.
+                The business magnate also shared with people how Alibaba has grown into what it is today.
+              </p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <img src="../../assets/img/p4.png" alt="">
+          <div class="comments">
+            <div class="name-date">
+              <div class="name">user的名子</div>
+              <div class="date">2017-09-18 14:58</div>
+            </div>
+            <div class="comment-words">
+              <p>The business magnate also shared with people how Alibaba has grown into what it is today.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <img src="../../assets/img/p7.png" alt="">
+          <div class="comments">
+            <div class="name-date">
+              <div class="name">user的名子</div>
+              <div class="date">2017-09-18 10:58</div>
+            </div>
+            <div class="comment-words">
+              <p>The business magnate also shared with people how Alibaba has grown into what it is today.</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style scoped>
+  .blog-comment{ margin: 0.2rem 0.2rem 0.7rem 0.2rem; }
+  .blog-comm-nav{ color: #5876a0; font-weight: bold;
+  border-bottom: 0.05rem solid #5876a0}
+  .user-comment ul li{ position: relative; padding: 0.2rem 0 0 0; display: inline-block;}
+  .user-comment img{ left: 0; top: 0; width: 1rem; height: 1rem; float: left; margin: 0.2rem 0.2rem 0 0;
+    text-align: center; border-radius: 50%;}
+  .comments {width:4.8rem; float: right; border-bottom: 0.02rem solid #5876a0}
+  .comments .name{ float: left;}
+  .comments .date{ float: right;}
+</style>
--- src/components/index/Blog.vue
+++ src/components/index/Blog.vue
@@ -2,6 +2,7 @@
   <div class="blog">
     <blog-head></blog-head>
     <blog-detail></blog-detail>
+    <blog-comment></blog-comment>
     <blog-foot></blog-foot>
   </div>
 </template>
@@ -14,11 +15,13 @@
   import BlogHead from '../common/BlogHead.vue'
   import BlogDetail from '../common/BlogDetail.vue'
   import BlogFoot from '../common/BlogFoot.vue'
+  import BlogComment from '../common/BlogComment.vue'
 
   export default {
     components:{
       BlogHead,
       BlogDetail,
+      BlogComment,
       BlogFoot
     }
   }
