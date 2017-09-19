f13bf87f7fd702ef59d88ad723651abd72f5a9d1
LQJIN <isjinliqiang@foxmail.com>
2017-09-19 16:43:36 +0800

    update BlogHead.vue

--- src/components/common/BlogHead.vue
+++ src/components/common/BlogHead.vue
@@ -9,17 +9,14 @@
 </template>
 
 <style scoped>
-  .blog-head { height: 0.9rem; color: #ffffff;
-    position: fixed; top: 0; width: 100%; background: #ffffff; }
+  .blog-head { height: 0.9rem; position: fixed; top: 0; width: 100%; background: #ffffff; }
   .blog-head { border-bottom: 0.02rem solid #a2b7b8}
 
   .blog-head ul li { display: inline-block; width: 0.8rem; height: 0.8rem; line-height: 0.8rem;
-    text-align: center; font-size: 0.5rem; color: #9d9d9d; padding-top: 0.1rem;}
+    text-align: center; font-size: 0.5rem; color: #0d4473; padding-top: 0.1rem;}
 
-  .blog-head ul li:nth-child(3){
-    float: right;}
-  .blog-head ul li:nth-child(2){
-    float: right;}
+  .blog-head ul li:nth-child(3){  float: right;}
+  .blog-head ul li:nth-child(2){  float: right;}
 </style>
 
 <script>
