dcb86c6fbcd2a7a46ae0feea1b090be11094a2c2
LQJIN <isjinliqiang@foxmail.com>
2017-09-25 18:17:43 +0800

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Index from '@/components/index/index.vue'
 import Blog from '@/components/index/Blog.vue'
 import BlogCommentAdd from '@/components/common/BlogCommentAdd.vue'
-import Me from '@/components/index/Me.vue'
+import Me from '@/components/index/MeSignIn.vue'
 
 Vue.use(Router);
 
