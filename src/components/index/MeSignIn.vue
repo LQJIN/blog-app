b63303fae8726fee7098e0d0a6b06eebed1ec42a
LQJIN <isjinliqiang@foxmail.com>
2017-09-26 14:44:57 +0800

    update MeSignIn.vue -- checked

--- src/components/index/MeSignIn.vue
+++ src/components/index/MeSignIn.vue
@@ -14,7 +14,7 @@
               <input type="password" name="pass" placeholder="Password"/>
             </div>
           <div class="keep-sign">
-            <span class="icon-lock"></span>
+            <span v-on:click="toggle()" v-bind:class="{'icon-check':isCheck,'icon-check-empty':!isCheck}"></span>
             <p>Keep me signed in</p>
           </div>
          <input class="button" type="submit" value="Continue"/>
@@ -26,8 +26,8 @@
       <div class="other-account">
         <p>Sign in with other accounts</p>
         <ul>
-          <li><img src="" alt=""><p>wechat</p></li>
-          <li><img src="" alt=""><p>weibo</p></li>
+          <li><img src="../../assets/img/wechat1.png" alt="wechat"><p>wechat</p></li>
+          <li><img src="../../assets/img/weibo.png" alt="weibo"><p>weibo</p></li>
         </ul>
       </div>
     </div>
@@ -61,6 +61,10 @@
   .sign-content .keep-sign p{display: block; width: 5rem; float: right;}
   .forget-password{text-align: center; border: none; height: 1rem; line-height: 1rem; font-size: 0.25rem;}
   .forget-password a{ color: #1c4d9c; text-decoration-line: underline;}
+  .other-account ul{display: flex; padding: 0 0.6rem;}
+  .other-account ul li{flex: 1;}
+  .other-account img{width: 1.2rem; height: 1.2rem;}
+  .other-account p{color: #ffffff; font-size: 0.25rem;}
 </style>
 
 <script>
@@ -68,15 +72,19 @@
   import Foot from '../common/foot.vue'
 
   export default {
-
+    data(){
+      return{
+        isCheck: true
+      }
+    },
     components: {
       MeHead,
       Foot
     },
-   /* methods: {
-      changeClass: function () {
-          this.d = 'false';
+    methods: {
+      toggle: function () {
+          this.isCheck = !this.isCheck;
       }
-    }*/
+    }
   }
 </script>
