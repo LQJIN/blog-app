0917c1cbe48ce534f8541cc9dc0d3ea01315cf2e
LQJIN <isjinliqiang@foxmail.com>
2017-09-26 16:50:38 +0800

    update MeHead.vue

--- src/components/common/MeHead.vue
+++ src/components/common/MeHead.vue
@@ -11,7 +11,7 @@
   .me-head { display: flex; height: 1rem; color: #fff; position: fixed; top: 0; width: 100%;
     background: #1649a9; border-bottom: 0.01rem solid #a2b7b8; z-index: 1;}
   .me-head span{ width: 1rem; height: 1rem; line-height: 1rem; text-align:center; font-size: 0.5rem; color: #ffffff; }
-  .me-head h2 { flex: 1; text-align: center; text-indent: 1rem; line-height: 1rem; font-size: 0.5rem;}
+  .me-head h2 { flex: 1; text-align: center; text-indent: 1.2rem; line-height: 1rem; font-size: 0.5rem;}
 </style>
 
 <script>
