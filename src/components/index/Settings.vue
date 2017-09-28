76d4808044146a5118960949404e7d0a21a0b0df
LQJIN <isjinliqiang@foxmail.com>
2017-09-28 11:30:39 +0800

    update Settings.vue

--- src/components/index/Settings.vue
+++ src/components/index/Settings.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="settings">
-
+    <div class="block">
+      <ul>
+        <li>Saved</li>
+        <li>Download</li>
+        <li>Dark</li>
+      </ul>
+    </div>
+    <div class="block">
+      <ul>
+        <li>Saved</li>
+        <li>Download</li>
+        <li>Dark</li>
+      </ul>
+    </div>
+    <div class="block">
+      <ul>
+        <li>Share This App</li>
+        <li>Help</li>
+        <li>About</li>
+        <li><mt-switch v-model="value">开关</mt-switch></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,5 +30,13 @@
 </style>
 
 <script>
+  import  Switch  from 'mint-ui';
+
+  export default {
+    components:{
+      Switch
+
+    }
+  }
 
 </script>
