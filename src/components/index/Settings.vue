9e5b144f9e493784e9ea93d21c081748ec9370af
LQJIN <isjinliqiang@foxmail.com>
2017-10-02 11:44:23 +0800

    update Settings.vue

--- src/components/index/Settings.vue
+++ src/components/index/Settings.vue
@@ -19,14 +19,17 @@
         <li>Share This App</li>
         <li>Help</li>
         <li>About</li>
-        <li><mt-switch v-model="value">开关</mt-switch></li>
+        <li v-on:click="change"><mt-switch v-model="value">开关</mt-switch></li>
       </ul>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+  .settings{ background: #ffffff;}
+  .settings .block{ background: #f7f7f7; border-bottom: 0.02rem solid #d7d7d7; border-top: 0.02rem solid #d7d7d7;margin: 0.4rem 0;}
+  .settings .block ul li{ width: 6rem; height: 1.4rem; line-height: 1.4rem;
+    color: #000000; border-bottom: 0.02rem solid #d7d7d7; padding-left: 0.4rem;}
 </style>
 
 <script>
@@ -34,7 +37,12 @@
   export default{
     data(){
       return{
-        value: true
+        value: false
+      }
+    },
+    methods:{
+      change:function () {
+        console.log('sssssss');
       }
     }
   }
