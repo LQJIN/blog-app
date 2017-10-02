a0522d7787759b0009d04e30a84c4df9a4419088
LQJIN <isjinliqiang@foxmail.com>
2017-10-02 10:44:27 +0800

    update BlogFoot.vue -- change foot icon

--- src/components/common/BlogFoot.vue
+++ src/components/common/BlogFoot.vue
@@ -5,19 +5,19 @@
         <a v-on:click="msgAdd()">
           <div>
             <h5>{{msg}}</h5>
-            <span class="glyphicon glyphicon-thumbs-up"></span>
+            <span class="icon-thumbs-up"></span>
           </div>
         </a>
       </li>
       <li>
         <router-link to="/comment">
           <h5>5</h5>
-          <span class="glyphicon glyphicon-comment"></span>
+          <span class=" icon-comments"></span>
         </router-link>
       </li>
       <li>
         <router-link to="/">
-          <span class="glyphicon glyphicon-share"></span>
+          <span class=" icon-share"></span>
         </router-link>
       </li>
     </ul>
@@ -29,12 +29,11 @@
 
   ul {display: flex; height: 0.7rem;line-height: 0.7rem;}
   ul li {flex: 1;  background-color: #ffffff; text-align: center; font-size: 0.3rem;}
-
+  ul li:nth-child(3){padding-top: 0.03rem;}
   .blog-foot a{display: inline-block; width: 100%;height: 100%; color: #1c4d9c; text-align:center;}
 
-  .blog-foot span{width: 0.45rem; height: 0.7rem; line-height: 0.7rem;  font-size: 0.36rem;}
-  .blog-foot h5{ display: inline-block; text-align: center; position: relative;
-    bottom: 0.09rem; font-size: 0.2rem;}
+  .blog-foot span{width: 0.45rem; height: 0.7rem; line-height: 0.7rem;  font-size: 0.4rem;}
+  .blog-foot h5{ display: inline-block; text-align: center; position: relative; font-size: 0.25rem;}
   /*解决了foot.vue和BlogFoot.vue中图标和文字不能对齐的问题。
       文字在<h5>标签中，给h5设置为inline-block行块，然后通过定位控制了文字与图标的位置。*/
 </style>
