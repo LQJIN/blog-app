813be8e0578732baf8ef4629c203ba74c3d8806d
LQJIN <isjinliqiang@foxmail.com>
2017-10-02 19:51:22 +0800

    update Settings.vue

--- src/components/index/Settings.vue
+++ src/components/index/Settings.vue
@@ -34,14 +34,14 @@
 <style scoped>
   .settings{ background: #ffffff;}
   .content-head { display: flex; height: 0.9rem; background: #ffffff; position: fixed; top: 0; width: 100%;
-    border-bottom: 0.02rem solid #d7d7d7;  z-index: 1;}
+    border-bottom: 0.01rem solid #d7d7d7;  z-index: 1;}
   .content-head span{ width: 0.9rem; height: 0.86rem; line-height: 0.86rem; text-align:center;
     font-size: 0.5rem; color: #0d4473; padding-top: 0.04rem;}
-  .content-head h4 { flex: 1; text-align: center; text-indent: -0.6rem; font-size: 0.5rem;
-    color: #0d4473; }
-  .settings .block{margin-top: 0.9rem;}
+  .content-head h4 { flex: 1; height: 0.86rem; line-height: 0.86rem; text-align: center; text-indent: -0.6rem; font-size: 0.36rem;
+    color: #0d4473; padding-top: 0.04rem;}
+  .settings .block{margin-top: 1.3rem;}
   .settings .block1{ background: #f7f7f7; border-bottom: 0.02rem solid #d7d7d7;
-    border-top: 0.02rem solid #d7d7d7;margin: 0.4rem 0;}
+    border-top: 0.02rem solid #d7d7d7; margin:  0 0 0.4rem 0;}
   .settings .block1 ul li{ width: 6rem; height: 1.4rem; line-height: 1.4rem;
     color: #000000; border-bottom: 0.02rem solid #d7d7d7; margin-left: 0.4rem; font-size: 0.4rem;}
   .settings .block1 .switch span{font-size: 0.1rem;}
