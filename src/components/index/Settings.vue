a2eef9e3eafb8fef0d7643c98fa011b521f4ff1c
LQJIN <isjinliqiang@foxmail.com>
2017-10-02 16:30:35 +0800

    update Settings.vue

--- src/components/index/Settings.vue
+++ src/components/index/Settings.vue
@@ -19,7 +19,7 @@
         <li>Share This App</li>
         <li>Help</li>
         <li>About</li>
-        <li v-on:click="change"><mt-switch v-model="value">开关</mt-switch></li>
+        <li v-on:change="change"><mt-switch class="switch" v-model="value">开关</mt-switch></li>
       </ul>
     </div>
   </div>
@@ -30,7 +30,8 @@
   .settings .block{ background: #f7f7f7; border-bottom: 0.02rem solid #d7d7d7;
     border-top: 0.02rem solid #d7d7d7;margin: 0.4rem 0;}
   .settings .block ul li{ width: 6rem; height: 1.4rem; line-height: 1.4rem;
-    color: #000000; border-bottom: 0.02rem solid #d7d7d7; margin-left: 0.4rem;}
+    color: #000000; border-bottom: 0.02rem solid #d7d7d7; margin-left: 0.4rem; font-size: 0.4rem;}
+  .settings .block .switch span{font-size: 0.4rem;}
 </style>
 
 <script>
@@ -49,3 +50,6 @@
   }
 
 </script>
+
+<!--备注：
+1.mint-ui上的事件只能使用change，不能使用click。-->
