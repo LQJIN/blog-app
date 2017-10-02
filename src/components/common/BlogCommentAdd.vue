624d844bf0be307b01a49ed2c81c045b72cec5dd
LQJIN <isjinliqiang@foxmail.com>
2017-10-02 19:37:56 +0800

    update BlogCommentAdd.vue

--- src/components/common/BlogCommentAdd.vue
+++ src/components/common/BlogCommentAdd.vue
@@ -100,14 +100,12 @@
 
 <style scoped>
 
-  .content-head { display: flex; height: 0.9rem; background: #ffffff; position: fixed; top: 0; width: 100%;
-    border-bottom: 0.02rem solid #d7d7d7;  z-index: 1;}
+  .content-head { display: flex; height: 0.9rem; background: #ffffff; position: fixed; top: 0; width: 100%;  border-bottom: 0.02rem solid #d7d7d7;  z-index: 1;}
   .content-head span{ width: 0.9rem; height: 0.86rem; line-height: 0.86rem; text-align:center; font-size: 0.5rem; color: #0d4473; padding-top: 0.04rem;}
   .content-head h4 { flex: 1; height: 0.86rem; line-height: 0.86rem; text-align: center; text-indent: -0.6rem; font-size: 0.35rem; color: #0d4473; padding-top: 0.04rem;}
   .comment-li {margin: 1.02rem 0.2rem 1.2rem 0.2rem; }
   .comment-li ul li{ position: relative; padding: 0.2rem 0 0 0; display: inline-block; font-size: 0.2rem;}
-  .comment-li img{ left: 0; top: 0; width: 1rem; height: 1rem; float: left; margin: 0.1rem 0.2rem 0 0;
-    text-align: center; border-radius: 50%;}
+  .comment-li img{ left: 0; top: 0; width: 1rem; height: 1rem; float: left; margin: 0.1rem 0.2rem 0 0;  text-align: center; border-radius: 50%;}
 
   .comments {width:4.8rem; float: right; border-bottom: 0.02rem solid #d7d7d7; }
   .comment-words{ clear: both; margin-bottom: 0.4rem;}
