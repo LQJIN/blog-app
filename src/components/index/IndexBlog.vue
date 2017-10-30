41e7ae5b601ff30f51693471891288efd1d9cc0c
LQJIN <isjinliqiang@foxmail.com>
2017-10-30 20:55:48 +0800

    update IndexBlog.vue

--- src/components/index/IndexBlog.vue
+++ src/components/index/IndexBlog.vue
@@ -1,174 +1,52 @@
 <template>
   <div class="index-blog">
     <ul class="index-blog-ul">
-      <li class="index-blog-ulli">
+      <li class="index-blog-ulli" v-for="news in newsList">
         <router-link to="/blog">
           <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
+            <h5>{{news.title}}</h5>
             <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
+              <li class="tag">{{news.tag}}</li>
+              <li class="date">{{news.date}}</li>
               <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
             </ul>
           </div>
           <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship.</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
-          </div>
-        </router-link>
-      </li>
-      <li class="index-blog-ulli">
-        <router-link to="/blog">
-          <div class="text">
-            <h5>Alibaba's Jack Ma shares insight into innovation,entrepreneurship.</h5>
-            <ul>
-              <li class="tag">BUSINESS</li>
-              <li class="date">September 18,2017</li>
-              <li class="comment"><span class="glyphicon glyphicon-comment"></span> 5</li>
-            </ul>
-          </div>
-          <div class="img">
-            <img src="../../assets/img/p1.png" alt="" />
+            <img :src="news.images.small" alt="" />
           </div>
         </router-link>
       </li>
     </ul>
   </div>
 </template>
+<script>
+
+  import Axios from 'axios';
+  import $ from 'jquery'
+
+  export default {
+    data(){
+      return {
+        newsList:[],
+        show:false
+      }
+    },
+    mounted:function () {
+
+    },
+    methods:{
+      loadData(){
+        var lenght = this.newsList.length;
+        Axios.get().then((res)=>{
+          this.newsList = this.newsList.concat(res.data.subjects);
+          //concat()函数作用是连接数组。
+          this.show = false;
+        })
+      }
+    }
+  }
+</script>
+
 <style scoped>
   .index-blog{ padding: 0 0.2rem;}
   .index-blog-ulli{ display: inline-block; padding: 0.2rem 0; border-bottom: 0.02rem solid #f8f8f8;}
